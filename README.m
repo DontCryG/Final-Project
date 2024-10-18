6a73949e5d3be82a7386c360b70e45328f92cbc0
dontcry <tanattapat1996@gmail.com>
2024-10-18 16:20:24 +0700

    Final Project V1

--- README.m
+++ README.m
@@ -1,1 +1,26 @@
-# Final-Project
+## Final Project
+
+CS310 / 127G | Bangkok University
+
+
+## 🛠 How to setup
+
+Simply run the below commands.
+
+1. Install dependencies
+
+```sh
+pip install virtualenv
+```
+
+```sh
+pip install django
+```
+
+2. Start the development server
+
+cd to folder final and run command
+
+```sh
+python manage.py runserver
+```
